98fd56fa45d8b8ea4407a911be2658fcb05b39ab
internetseekho <internetseekho.com@gmail.com>
2017-05-24 20:29:31 +0500

    Code you can use in tutorial 16 and 17
    Code you can use in tutorial 16 and 17

---
+++ Assets/TextController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    // ---------------------------------------------------------------------------------
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "Subscribe to internetseekho my channel to recevive notifications";
+	}
+	
+    // ---------------------------------------------------------------------------------
+
+	// Update is called once per frame
+	void Update () {
+
+        if ( Input.GetKeyDown( KeyCode.Space ) ) {
+
+            text.text = "Like the video if you are learing new thing";
+
+        }
+
+	}
+
+    // ---------------------------------------------------------------------------------
+
+}
