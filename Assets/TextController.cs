fb51252edfe8336ed07ecb2739004a78b7c74ec8
internetseekho <internetseekho.com@gmail.com>
2017-06-02 18:30:48 +0500

    code you can use for video 18
    code you can use for video 18

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,28 +5,24 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour {
 
-    // ---------------------------------------------------------------------------------
-
     public Text text;
 
 	// Use this for initialization
 	void Start () {
-        text.text = "Subscribe to internetseekho my channel to recevive notifications";
-	}
-	
-    // ---------------------------------------------------------------------------------
 
+        text.text   = "You are being kidnaped by the criminals in a dark room but you are willing to escape from this place.\n\n";
+        text.text  += "To View Your Backpack press B, for Exploring the room press E and for sleeping press S.";
+
+    }
+	
 	// Update is called once per frame
 	void Update () {
 
-        if ( Input.GetKeyDown( KeyCode.Space ) ) {
+        if ( Input.GetKeyDown( "space" ) ) {
 
-            text.text = "Like the video if you are learing new thing";
+            text.text = "Like the video if you are learning new things";
 
         }
 
 	}
-
-    // ---------------------------------------------------------------------------------
-
 }
